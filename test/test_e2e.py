69cca85cd1c85d6d76529585367c1c07e01d5f64
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-01-14 00:58:51 +0100

    Fix tests

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -10,7 +10,7 @@ from src.env_info import env_to_action_type
 class E2E(unittest.TestCase):
     def test_runs_ok_in_every_env(self):
         def _run_in_env(env_id):
-            model = CMAESNN(env_id, cmaes_population_size=5, max_nn_params='minimal')
+            model = CMAESNN(env_id, max_nn_params='minimal')
             model.learn(total_timesteps=1)
             env = gym.make(env_id)
             obs = env.reset()
@@ -26,7 +26,7 @@ class E2E(unittest.TestCase):
 
     def test_reaches_end_of_env(self):
         env_id = 'CartPole-v0'
-        model = CMAESNN(env_id, cmaes_population_size=5, max_nn_params='minimal')
+        model = CMAESNN(env_id, max_nn_params='minimal')
         model.learn(total_timesteps=1)
         env = gym.make(env_id)
         obs = env.reset()
@@ -44,7 +44,7 @@ class E2E(unittest.TestCase):
         params_configs = ['minimal', 'standard', 100]
 
         def _train_save_load_run(_env_id, _config):
-            model = CMAESNN(_env_id, cmaes_population_size=5, max_nn_params=_config)
+            model = CMAESNN(_env_id, max_nn_params=_config)
             model.learn(total_timesteps=1)
             model.save(model_path)
             del model
