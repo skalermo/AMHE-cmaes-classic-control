336e985fe48d3312955c59546538a8b365159fc4
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-01-16 14:22:03 +0100

    Test log-extraction on cmaes nn logs

--- test/test_log_extraction.py
+++ test/test_log_extraction.py
@@ -4,7 +4,8 @@ import gym
 from stable_baselines3 import A2C, PPO
 
 from src.env_info import env_to_action_type
-from src.log_utils import process_logs
+from src.log_utils import process_logs, process_cmaess_nn_logs
+from src.cmaes_nn import CMAESNN
 from utils import captured_output
 
 
@@ -29,6 +30,16 @@ class TestLearningLogExtraction(unittest.TestCase):
                     model.learn(total_timesteps=100)
                 output = out.getvalue().strip()
                 data = process_logs(output)
+                self.assertTrue(len(data) > 0)
+
+    def test_cmaes_nn(self):
+        for env_id in self.envs:
+            with self.subTest(f'{env_id=}'):
+                with captured_output() as (out, err):
+                    model = CMAESNN(env_id, verbose=1)
+                    model.learn(total_timesteps=100, log_interval=1)
+                output = out.getvalue().strip()
+                data = process_cmaess_nn_logs(output)
                 self.assertTrue(len(data) > 0)
 
 
